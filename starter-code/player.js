438a353840f369ff06efe9ffde386486c3995b4c
4idaJV <aida.jv@hotmail.es>
2024-03-29 18:48:51 +0000

    se hace modificación en la operación de altura superior

--- starter-code/player.js
+++ starter-code/player.js
@@ -23,8 +23,13 @@ function Player (x, y, board) {
             self.y = newCoordY;
             self.sprite.style.top = self.y + 'px';
         } 
-/*         
-        if (newCoordY >= 750){
+        // else if (newCoordY > 750){
+        // player.isDead = true
+        // clearInterval(timerIdPlayer)
+
+        // }
+/*      
+        if (newCoordY > 750){
             player.isDead = true
             clearInterval(timerIdPlayer)
 
